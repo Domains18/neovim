ce4935ca5ff7017b3db1bdac559d3f8f94a54e05
Domains18 <gibsonsgibson88@gmail.com>
2023-02-25 23:03:50 +0300

    app setup

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const paypal = require('paypal-rest-sdk');
 const app = express();
 
 
+
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => res.render('index'));
+
+// app.set('views', path.join(__dirname, 'templates'));
 
 
 app.listen(3000, () => console.log('Server Started'));
